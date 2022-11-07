11e7e999d0103444aec773f083b500472cd33240
nirvana <bastienbaranoff@gmail.com>
2022-11-07 07:09:55 +0100

    commit

---
+++ lib/gcc/arm-elf/4.5.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/root/gnuarm/src/gcc-4.5.2/configure --target=arm-elf --prefix=/root/gnuarm/install --enable-interwork --enable-multilib --with-float=soft --disable-werror --enable-languages=c,c++ --with-newlib --with-headers=/root/gnuarm/src/newlib-1.19.0/newlib/libc/include --with-system-zlib --disable-shared";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "float", "soft" } };
